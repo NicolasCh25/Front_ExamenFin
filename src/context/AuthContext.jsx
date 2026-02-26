bc404beaa779a5b7a1cff2aa6bfbe293bf58de9b
Nicolas Ch <nicolas.chiguano@epn.edu.ec>
2026-02-26 12:40:41 -0500

    Actualizacion

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -20,7 +20,7 @@ export const AuthProvider = ({ children }) => {
     // LOGIN
     const login = async (email, password) => {
         try {
-            const { data } = await api.post('/auth/login', { email, password });
+            const { data } = await api.post('/usuario/login', { email, password });
 
             localStorage.setItem('token', data.token);
             localStorage.setItem('user', JSON.stringify(data.user));
